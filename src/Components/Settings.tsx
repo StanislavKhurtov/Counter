894e80143baa3ba3da90fc820c6da0dc498090b5
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-06-26 20:42:43 +0300

    feat: add function

--- src/Components/Settings.tsx
+++ src/Components/Settings.tsx
@@ -1,28 +1,63 @@
+//import React, {ChangeEvent, useState} from 'react';
+//import {Button} from "../Button";
+//
+//export const Settings = () => {
+//    let [maxValue, setMaxValue] = useState<number>(0)
+//    let [minValue, setMinValue] = useState<number>(0)
+//
+//    return (
+//        <div className={"counter"}>
+//            <div className={"itemsInput"}>
+//                <input type="number" value={maxValue} onChange={(e: ChangeEvent<HTMLInputElement>) => {
+//                    console.log(e.currentTarget.value)
+//                    let valueMaxString = JSON.parse(e.currentTarget.value)
+//                    setMaxValue(valueMaxString)
+//                }}/>
+//                <input type="number" value={minValue} onChange={(e: ChangeEvent<HTMLInputElement>) => {
+//                    console.log(e.currentTarget.value)
+//                    let valueMinString = JSON.parse(e.currentTarget.value)
+//                    setMinValue(valueMinString)
+//                }}/>
+//            </div>
+//            <div className={"buttons"}>
+//                <Button name="set" callback={() => {
+//                    console.log(localStorage.getItem('counterValue'))
+//
+//                }}/>
+//            </div>
+//
+//        </div>
+//    );
+//};
+
+
+
+
 import React, {ChangeEvent, useState} from 'react';
 import {Button} from "../Button";
 
 export const Settings = () => {
-    let [maxValue, setMaxValue] = useState<string>("")
-    let [minValue, setMinValue] = useState<string>("")
+    const [maxValue, setMaxValue] = useState<number>(0);
+    const [minValue, setMinValue] = useState<number>(0);
+
+    const handleSetClick = () => {
+        localStorage.setItem('minCount', JSON.stringify(minValue));
+        localStorage.setItem('maxCount', JSON.stringify(maxValue));
+    };
 
     return (
         <div className={"counter"}>
             <div className={"itemsInput"}>
-                <input type="number" value={maxValue} onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                    console.log(e.currentTarget.value)
-                    setMaxValue(e.currentTarget.value)
-                }}/>
-                <input type="number" value={minValue} onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                    console.log(e.currentTarget.value)
-                    setMinValue(e.currentTarget.value)
+                <input type="text"  value={minValue} onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    setMinValue(Number(e.currentTarget.value));
+                }} />
+                <input type="text" value={maxValue} onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    setMaxValue(Number(e.currentTarget.value));
                 }}/>
             </div>
             <div className={"buttons"}>
-                <Button name="set" callback={() => {
-                }}/>
+                <Button name="set" callback={handleSetClick}/>
             </div>
-
         </div>
     );
 };
-
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,46 +1,88 @@
+//import React, {useEffect, useState} from 'react';
+//import {Button} from "./Button";
+//
+//export const Counter = () => {
+//
+//    const minCount = 0;
+//    const maxCount = 5;
+//
+//
+//    const [value, setValue] = useState<number>(() => {
+//        const valueAsString = localStorage.getItem('counterValue')
+//        if (valueAsString) {
+//            return JSON.parse(valueAsString)
+//        } else {
+//            return minCount
+//        }
+//    });
+//
+//
+//    useEffect(() => {
+//        localStorage.setItem('counterValue', JSON.stringify(value))
+//    }, [value])
+//
+//
+//    const increment = () => {
+//        if (value < maxCount) {
+//            setValue(prevValue => prevValue + 1);
+//        }
+//    };
+//
+//    const reset = () => {
+//        setValue(minCount);
+//
+//    };
+//
+//    return (
+//        <div className={"counter"}>
+//            <div className={value < maxCount ? "display" : "active"}>{value}</div>
+//            <div className={"buttons"}>
+//                <Button name={"Inc"} callback={increment} disabled={value === maxCount}/>
+//                <Button name={"Reset"} callback={reset} disabled={value === minCount}/>
+//            </div>
+//        </div>
+//    );
+//}
+
+
+
 import React, {useEffect, useState} from 'react';
 import {Button} from "./Button";
 
 export const Counter = () => {
-
-    const minCount = 0;
-    const maxCount = 5;
-
-
     const [value, setValue] = useState<number>(() => {
-        const valueAsString = localStorage.getItem('counterValue')
+        const valueAsString = localStorage.getItem('counterValue');
         if (valueAsString) {
-            return JSON.parse(valueAsString)
+            return JSON.parse(valueAsString);
         } else {
-            return minCount
+            return 0;
         }
     });
 
+    const minCount = Number(localStorage.getItem('minCount')) || 0;
+    const maxCount = Number(localStorage.getItem('maxCount')) || 5;
 
     useEffect(() => {
-        localStorage.setItem('counterValue', JSON.stringify(value))
-    }, [value])
+        localStorage.setItem('counterValue', JSON.stringify(value));
+    }, [value]);
 
-
-    const increment = () => {
+    const handleIncrementClick = () => {
         if (value < maxCount) {
             setValue(prevValue => prevValue + 1);
         }
     };
 
-    const reset = () => {
+    const handleResetClick = () => {
         setValue(minCount);
-
     };
 
     return (
         <div className={"counter"}>
             <div className={value < maxCount ? "display" : "active"}>{value}</div>
             <div className={"buttons"}>
-                <Button name={"Inc"} callback={increment} disabled={value === maxCount}/>
-                <Button name={"Reset"} callback={reset} disabled={value === minCount}/>
+                <Button name={"Inc"} callback={handleIncrementClick} disabled={value === maxCount}/>
+                <Button name={"Reset"} callback={handleResetClick} disabled={value === minCount}/>
             </div>
         </div>
     );
-}
-
+};
